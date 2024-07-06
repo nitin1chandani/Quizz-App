f617797ec0949215fe8ae6ff72de2ab2d942043b
nitin1chandani <nitin1chandani@gmail.com>
2024-07-06 18:20:46 +0530

    Added: Some unit tests

---
+++ src/test/java/com/nitin/quizzapp/service/QuestionServiceTest.java
@@ -0,0 +1,107 @@
+package com.nitin.quizzapp.service;
+
+import com.nitin.quizzapp.model.DTO.QuestionDetailsRequestDTO;
+import com.nitin.quizzapp.model.DTO.QuestionDetailsResponseDTO;
+import com.nitin.quizzapp.model.Question;
+import com.nitin.quizzapp.repo.QuestionRepo;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class QuestionServiceTest {
+    @Mock
+    private QuestionRepo questionRepo;
+    @InjectMocks
+    private QuestionService questionService;
+
+
+    @Test
+    public void shouldReturnAllQuestions(){
+        Question question = Question.builder()
+                .category("coding")
+                .difficultyLevel("medium")
+                .questionTitle("What is an example of a bubble sort algorithm?")
+                .rightAnswer("Comparing adjacent elements and swapping them if they are in the wrong order.")
+                .option1("Iteratively searching for the largest element and placing it in its correct position.")
+                .option2("Recursively dividing the array into halves until only one element remains.")
+                .option3("Using a stack to reverse the order of elements in an array.")
+                .option4("Sorting elements by comparing them with a pivot and recursively sorting the subarrays.")
+                .build();
+
+        when(questionRepo.findAll()).thenReturn(List.of(question));
+
+        List<QuestionDetailsResponseDTO> result = questionService.getAllQuestions();
+        assertEquals(result.size(), 1);
+        assertEquals(result.get(0).getCategory(), "coding");
+    }
+
+    @Test
+    public void shouldGetQuestionById(){
+        Question question = Question.builder()
+                .id(1l)
+                .category("coding")
+                .difficultyLevel("medium")
+                .questionTitle("What is an example of a bubble sort algorithm?")
+                .rightAnswer("Comparing adjacent elements and swapping them if they are in the wrong order.")
+                .option1("Iteratively searching for the largest element and placing it in its correct position.")
+                .option2("Recursively dividing the array into halves until only one element remains.")
+                .option3("Using a stack to reverse the order of elements in an array.")
+                .option4("Sorting elements by comparing them with a pivot and recursively sorting the subarrays.")
+                .build();
+
+        when(questionRepo.findById(1l)).thenReturn(Optional.ofNullable(question));
+        QuestionDetailsResponseDTO result = questionService.getQuestionById(1l);
+        assertEquals(result.getCategory(), "coding");
+    }
+
+
+    // TODO
+//    @Test
+//    public void shouldAddQuestionInDatabase(){
+//        // Sample data for the question
+//        Question question = Question.builder()
+//                .id(1L)
+//                .category("coding")
+//                .difficultyLevel("medium")
+//                .questionTitle("What is an example of a bubble sort algorithm?")
+//                .rightAnswer("Comparing adjacent elements and swapping them if they are in the wrong order.")
+//                .option1("Iteratively searching for the largest element and placing it in its correct position.")
+//                .option2("Recursively dividing the array into halves until only one element remains.")
+//                .option3("Using a stack to reverse the order of elements in an array.")
+//                .option4("Sorting elements by comparing them with a pivot and recursively sorting the subarrays.")
+//                .build();
+//
+//        // Mock behavior of repository save method
+//        when(questionRepo.save(question)).thenReturn(question);
+//
+//        // Create DTO object for service method
+//        QuestionDetailsRequestDTO questionDetailsRequestDTO = QuestionDetailsRequestDTO.builder()
+//                .category("coding")
+//                .difficultyLevel("medium")
+//                .questionTitle("What is an example of a bubble sort algorithm?")
+//                .rightAnswer("Comparing adjacent elements and swapping them if they are in the wrong order.")
+//                .option1("Iteratively searching for the largest element and placing it in its correct position.")
+//                .option2("Recursively dividing the array into halves until only one element remains.")
+//                .option3("Using a stack to reverse the order of elements in an array.")
+//                .option4("Sorting elements by comparing them with a pivot and recursively sorting the subarrays.")
+//                .build();
+//
+//        // Call the service method to add the question
+//        Question savedQuestion = questionService.addQuestion(questionDetailsRequestDTO);
+//
+//        // Assert that the returned question matches the expected question title
+//        assertEquals(question.getCategory(), savedQuestion.getCategory());
+//    }
+
+
+
+}
